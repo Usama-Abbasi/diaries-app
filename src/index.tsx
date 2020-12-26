a18fe1fc7b510625b7b2d6afa297ba83b383b4d9
usama999abbasi <60782977+usama999abbasi@users.noreply.github.com>
2020-12-26 18:24:36 +0500

    Updated

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,9 @@ import { setupServer } from './services/mirage/server';
 import { Provider } from 'react-redux';
 import store from './store';
 import reportWebVitals from './reportWebVitals';
-// if (process.env.NODE_ENV === 'development') {
-//   setupServer();
-// }
+
+setupServer();
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
